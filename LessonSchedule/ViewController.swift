627b551f9ddc12a4944c8b76c8da33f7994df533
IsaikinSergei <akinfest007@gmail.com>
2021-07-18 17:34:41 +0400

    Initial Commit

---
+++ LessonSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonSchedule
+//
+//  Created by Sergei Isaikin on 18.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
