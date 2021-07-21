994346b0143ae130a8cc6e280b21eb8504f5c6e4
IsaikinSergei <akinfest007@gmail.com>
2021-07-21 23:46:58 +0400

    Added overrideUserInterfaceStyle = .light

--- LessonSchedule/AppDelegate.swift
+++ LessonSchedule/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
+        window?.overrideUserInterfaceStyle = .light
         
         return true
     }
